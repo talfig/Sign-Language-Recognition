2cd8f1623d4c16be89bbdbf3fde63126afece24a
talfig <talfig8@gmail.com>
2024-10-11 00:05:16 +0300

    Updating requirements.txt

---
+++ piccompressor/__init__.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import numpy as np
+
+
+# Resize and save images in compressed .npz format
+def preprocess_and_save(dataset_dir, output_file, size=(200, 200)):
+    images = []
+
+    for filename in os.listdir(dataset_dir):
+        filepath = os.path.join(dataset_dir, filename)
+
+        # Read and resize image
+        image = cv2.imread(filepath)
+        resized_image = cv2.resize(image, size)
+
+        # Add to list of images
+        images.append(resized_image)
+
+    # Convert to numpy array and save
+    images_np = np.array(images)
+    np.savez_compressed(output_file, images_np)
+
+
+# Usage example
+preprocess_and_save('path_to_dataset', 'compressed_dataset.npz')
