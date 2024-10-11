1d37f421d4e82b4e2d5db218848d0677984a2960
talfig <talfig8@gmail.com>
2024-10-11 10:58:57 +0300

    Updating compressor package

---
+++ compressor/__init__.py
@@ -0,0 +1,9 @@
+# mypackage/__init__.py
+
+
+# Import specific functions from submodules to make them accessible directly
+from .datacompressor import compress2npz
+
+
+def init():
+    compress2npz('path_to_dataset', 'compressed_dataset.npz', size=(200, 200))
