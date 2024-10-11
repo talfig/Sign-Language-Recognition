b97c1039ca8a9025c055e878ca469302bbb076ba
talfig <talfig8@gmail.com>
2024-10-11 12:34:07 +0300

    Removed dataCompressor old package

--- dataCompressor/__init__.py
+++
@@ -1,9 +0,0 @@
-# mypackage/__init__.py
-
-
-# Import specific functions from submodules to make them accessible directly
-from .compressor import compress2npz
-
-
-def init():
-    compress2npz('path_to_dataset', 'compressed_dataset.npz', size=(200, 200))
