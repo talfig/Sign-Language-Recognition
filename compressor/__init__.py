d479aa0a71c3ed905662b6b5c914b8e7f26b09ce
talfig <talfig8@gmail.com>
2024-10-16 14:54:28 +0300

    Updating compressor init

--- compressor/__init__.py
+++ compressor/__init__.py
@@ -6,4 +6,4 @@ from compressor.data_compressor import *
 
 # Call the init function when the module is run as a script
 if __name__ == "__main__":
-    compress2npz('C:/Users/xbpow/Downloads/Sign-Language-Recognition/ASL-crop', '../data/compressed_asl_crop.npz')
+    compress2npz('../ASL-crop', '../data/compressed_asl_crop.npz')
