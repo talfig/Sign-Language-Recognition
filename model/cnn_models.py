3fd3985e312b0b9f8cdc7cf210050322162305fc
talfig <talfig8@gmail.com>
2024-10-21 17:25:43 +0300

    Updated documentation

--- model/cnn_models.py
+++ model/cnn_models.py
@@ -35,7 +35,7 @@ class CustomResNet18(nn.Module):
         # Load the pre-trained ResNet50 model
         self.base_model = resnet18(weights=ResNet18_Weights.DEFAULT)
 
-        # Modify the fully connected layer (classifier part) of ResNet50
+        # Modify the fully connected layer (classifier part) of ResNet18
         self.base_model.fc = nn.Sequential(
             nn.Linear(self.base_model.fc.in_features, 1280),  # Change output size to 1280
             nn.ReLU(),
