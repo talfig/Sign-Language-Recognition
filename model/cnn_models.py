1a57efd53ceb741fdd97dafb5d044b5ccf996162
talfig <talfig8@gmail.com>
2024-11-06 09:25:53 +0200

    Addint the new custom CNN class

--- model/cnn_models.py
+++ model/cnn_models.py
@@ -4,10 +4,63 @@ import torch
 import string
 import torch.nn as nn
 from torchsummary import summary
+import torch.nn.functional as F
 from torchvision.models import (
     mobilenet_v2, MobileNet_V2_Weights,
     resnet18, ResNet18_Weights
 )
+
+
+class CustomCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CustomCNN, self).__init__()
+
+        # Convolutional layers with added batch normalization and dropout
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        # Dropout after pooling layers
+        self.dropout_conv = nn.Dropout(0.2)
+
+        # Fully connected layers with dropout
+        self.fc1 = nn.Linear(256 * 14 * 14, 1024)  # Adjusted for additional conv layer
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, num_classes)
+        self.dropout_fc = nn.Dropout(0.4)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.dropout_conv(x)
+
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.dropout_conv(x)
+
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.dropout_conv(x)
+
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = self.dropout_conv(x)
+
+        x = x.view(-1, 256 * 14 * 14)  # Flatten for fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.dropout_fc(x)
+
+        x = F.relu(self.fc2(x))
+        x = self.dropout_fc(x)
+
+        x = self.fc3(x)
+        return x
 
 
 class CustomMobileNetV2(nn.Module):
